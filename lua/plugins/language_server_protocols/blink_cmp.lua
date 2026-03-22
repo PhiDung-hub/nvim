2f6d84a3e74f4dd2a07c6db63dc7869478e7ad46
phil <philho.dev@outlook.com>
2026-03-22 21:41:28 +0800

    feat: replace nvim-cmp with blink.cmp
    - Faster completion engine with built-in fuzzy, icons, and signature help
    - Remove lspkind-nvim (blink.cmp renders kind icons natively)

---
+++ lua/plugins/language_server_protocols/blink_cmp.lua
@@ -0,0 +1,46 @@
+return {
+  "saghen/blink.cmp",
+  version = "1.*",
+  event = { "InsertEnter", "CmdlineEnter" },
+  dependencies = {
+    "rafamadriz/friendly-snippets",
+  },
+  opts = {
+    keymap = {
+      preset = "none",
+      ["<C-n>"] = { "select_next", "fallback" },
+      ["<C-p>"] = { "select_prev", "fallback" },
+      ["<CR>"] = { "accept", "fallback" },
+      ["<C-e>"] = { "cancel", "fallback" },
+      ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+      ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+    },
+    sources = {
+      default = { "lsp", "path", "snippets", "buffer" },
+    },
+    completion = {
+      menu = {
+        max_height = 10,
+        draw = {
+          columns = { { "kind_icon" }, { "label", "label_description", gap = 1 }, { "source_name" } },
+        },
+      },
+      documentation = {
+        auto_show = true,
+        auto_show_delay_ms = 200,
+      },
+      list = {
+        selection = { preselect = false, auto_insert = false },
+      },
+    },
+    signature = {
+      enabled = true,
+    },
+    appearance = {
+      nerd_font_variant = "mono",
+    },
+    cmdline = {
+      enabled = true,
+    },
+  },
+}
